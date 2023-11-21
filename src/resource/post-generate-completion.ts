c7dcde353540d59c413f04093dc7e3aede9048c3
Alexandre <alexandreaddres@gmail.com>
2023-11-21 05:51:16 -0300

    mvp testing and done.

--- src/resource/post-generate-completion.ts
+++ src/resource/post-generate-completion.ts
@@ -13,11 +13,11 @@ export async function postGenerateCompletionResource(app: FastifyInstance){
 
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5)
         })
 
-        const {videoId, template, temperature} = bodySchema.parse(req.body)
+        const {videoId, prompt, temperature} = bodySchema.parse(req.body)
 
         const video = await prisma.videoInformation.findFirstOrThrow({
             where: {
@@ -31,7 +31,7 @@ export async function postGenerateCompletionResource(app: FastifyInstance){
             })
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription)
+        const promptMessage = prompt.replace('{transcription}', video.transcription)
 
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
